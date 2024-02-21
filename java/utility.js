6140b3a2634fc17d72a40b6d52fe14171444a204
siddiqrajib <rajibtextile32@gmail.com>
2024-02-21 13:12:09 +0600

    final

--- java/utility.js
+++ java/utility.js
@@ -1,19 +1,71 @@
-function play(){
-    const alphahet = getRandomAlphabet();
-    console.log('your random alphabet',alphahet);
+var remainingSeats = 28;
+var bookedSeats = 0;
+let TotalPrice =0;
 
-
-
-
-
-
- 
+function toSeat(seat) {
+    if (seat.style.backgroundColor === "green") {
+        seat.style.backgroundColor = "white";
+        remainingSeats++;
+        bookedSeats--;
+        TotalPrice -= 500;
+    } else {
+        seat.style.backgroundColor = "green";
+        remainingSeats--;
+        bookedSeats++;
+        TotalPrice += 500;
+    }
+    document.getElementById("remainingSeats").innerText = remainingSeats;
+    document.getElementById("bookedSeats").innerText = bookedSeats;
+    document.getElementById("totalPrice").innerText = "$" + totalPrice + " (" + seattextContent + ")";
 }
 
-for(const btn of alphahet){
-    console.log(btn);  
-    
-    
-    const button = document
+
+
+const allbtn=document.getElementsByClassName("kbd");
+for(const btn of allbtn){
+    btn.addEventListener("click",function(e){
+        const seatName = e.target.parentNode.childNodes[3].innerText;
+        const Pricef = document.getElementById("pricefind");
+        const maxprice =Pricef.parentElement.innerText;
+        const selectedArea = document.getElementById("placeseat");
+
+        const li = document.createElement('li');
+
+        const p = document.createElement('p');
+        p.innerText = seatName;
+
+        const p2 = document.createElement('p2');
+        p2.innerText = maxprice;
+
+        li.appendChild(p);
+        li.appendChild(p2);
+        selectedArea.appendChild(li);
+
+
+        const totalcost = document.getElementById("TotalPrice").innerText;
+        
+        const convertedtotalprice = parseInt(totalcost);
+      
+        document.getElementById("TotalPrice").innerText = convertedtotalprice+parseInt(maxprice);
+
+
+    })
 }
 
+
+
+ const btn = document.getElementById("apply-btn");
+ btn.addEventListener("click",function(){
+    const inputfield = document.getElementById("input-field").value ;
+    console.log(inputfield);
+    const couponcode = inputfield.split(" ").join("").toUpperCase();
+    console.log(couponcode);
+    if(totalcost>=550){
+        if(couponcode==="sell200"){
+            console.log('Rajib')
+        }else{
+            alert();
+        }
+    }
+
+ })
